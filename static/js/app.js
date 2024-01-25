db2fb4a4012240cd69625839166abfe127287f07
INOPIAE <m.maengel@inopiae.de>
2024-01-25 04:50:06 +0100

    modification of signaturpad

--- static/js/app.js
+++ static/js/app.js
@@ -13,7 +13,7 @@ const canvas = wrapper.querySelector("canvas");
 const signaturePad = new SignaturePad(canvas, {
   // It's Necessary to use an opaque color when saving image as JPEG;
   // this option can be omitted if only saving as PNG or SVG
-  backgroundColor: 'rgb(255, 255, 255)'
+  backgroundColor: 'rgb(255,245,238)'
 });
 
 // Adjust canvas coordinate space taking into account pixel ratio,
