8f44f158e0774b23f0df88f220bcd5bd0e610403
INOPIAE <m.maengel@inopiae.de>
2024-01-26 06:12:38 +0100

    add: page to show content of folder upload/signature

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from flask import  flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from datetime import datetime
 
+from flask import abort,  send_file
+from werkzeug.utils import safe_join
+from pathlib import Path
+import datetime as dt
+
 
 UPLOAD_FOLDER = './upload'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'svg'}
@@ -11,13 +16,6 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'svg'}
 app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-messages = [{'title': 'Message One',
-             'content': 'Message One Content'},
-            {'title': 'Message Two',
-             'content': 'Message Two Content'}
-            ]
-
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -29,10 +27,29 @@ def uniqueFilename(filename):
     fname=filename.rsplit('.', 1)
     return fname[0] + '_' + str(unix_timestamp) + '.' + fname[1]
 
+def getReadableByteSize(num, suffix='B') -> str:
+    for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Y', suffix)
+
+def getTimeStampString(tSec: float) -> str:
+    tObj = dt.datetime.fromtimestamp(tSec)
+    tStr = dt.datetime.strftime(tObj, '%Y-%m-%d %H:%M:%S')
+    return tStr
+
+def getIconClassForFilename(fName):
+    fileExt = Path(fName).suffix
+    fileExt = fileExt[1:] if fileExt.startswith(".") else fileExt
+    fileTypes = ["aac", "ai", "bmp", "cs", "css", "csv", "doc", "docx", "exe", "gif", "heic", "html", "java", "jpg", "js", "json", "jsx", "key", "m4p", "md", "mdx", "mov", "mp3",
+                 "mp4", "otf", "pdf", "php", "png", "pptx", "psd", "py", "raw", "rb", "sass", "scss", "sh", "sql", "svg", "tiff", "tsx", "ttf", "txt", "wav", "woff", "xlsx", "xml", "yml"]
+    fileIconClass = f"bi bi-filetype-{fileExt}" if fileExt in fileTypes else "bi bi-file-earmark"
+    return fileIconClass
 
 @app.route('/')
 def index():
-    return render_template('index.html', messages=messages)
+    return render_template('index.html')
 
 
 @app.route('/upload', methods=['GET', 'POST'])
@@ -53,3 +70,36 @@ def upload():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'],'signature', uniqueFilename(filename) ))
             return render_template('download_file.html', fname=filename)
     return render_template('upload.html')
+
+# route handler
+@app.route('/reports/', defaults={'reqPath': ''})
+@app.route('/reports/<path:reqPath>')
+def reports(reqPath):
+    FolderPath = os.path.join(app.config['UPLOAD_FOLDER'],'signature')
+    # Join the base and the requested path
+    # could have done os.path.join, but safe_join ensures that files are not fetched from parent folders of the base folder
+    absPath = safe_join(FolderPath, reqPath)
+
+    # Return 404 if path doesn't exist
+    if not os.path.exists(absPath):
+        return abort(404)
+
+    # Check if path is a file and serve
+    if os.path.isfile(absPath):
+        return send_file(absPath)
+
+    # Show directory contents
+    def fObjFromScan(x):
+        fileStat = x.stat()
+        # return file information for rendering
+        return {'name': x.name,
+                'fIcon': "bi bi-folder-fill" if os.path.isdir(x.path) else getIconClassForFilename(x.name),
+                'relPath': os.path.relpath(x.path, FolderPath).replace("\\", "/"),
+                'mTime': getTimeStampString(fileStat.st_mtime),
+                'size': getReadableByteSize(fileStat.st_size)}
+    fileObjs = [fObjFromScan(x) for x in os.scandir(absPath)]
+    # get parent directory url
+    parentFolderPath = os.path.relpath(
+        Path(absPath).parents[0], FolderPath).replace("\\", "/")
+    return render_template('file_list.html', data={'files': fileObjs,
+                                                 'parentFolder': parentFolderPath})
--- installpath.md
+++ installpath.md
@@ -6,3 +6,4 @@ https://github.com/szimek/signature_pad
 https://cdn.jsdelivr.net/npm/signature_pad@4.1.7/dist/signature_pad.umd.min.js
 https://szimek.github.io/signature_pad/js/app.js
 
+https://stackoverflow.com/questions/18334717/how-to-upload-a-file-using-an-ajax-call-in-flask/20107080#20107080
